d188fcefe039f569d86f8854ba240e0e6e766b50
conda act pt <vscode@163.com>
2020-07-12 19:46:20 +0800

    add mouse check

--- openCV/Finding-Angles-In-Image/mousePoints.py
+++ openCV/Finding-Angles-In-Image/mousePoints.py
@@ -1,24 +1,31 @@
 import cv2
-import math
+# import math
+###################################
+widthImg=540
+heightImg =640
+#####################################
 
 
-path = 'angle.png'
-img = cv2.imread(path)
-pointsList = []
+img = cv2.imread("angle.png")
+
+# img = cv2.resize(img,(widthImg,heightImg))
+# pointsList = []
+cv2.imshow('Image',img)
+cv2.waitKey(0)
+
+# def mousePoints(event,x,y,flags,params):
+#     if event == cv2.EVENT_LBUTTONDOWN:
+#         cv2.circle(img, (x, y), 5, (0, 0, 255), cv2.FILLED)
+#         pointsList.append([x,y])
+#         print(pointsList)
 
 
-def mousePoints(event,x,y,flags,params):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        cv2.circle(img, (x, y), 5, (0, 0, 255), cv2.FILLED)
-        pointsList.append([x,y])
-        print(pointsList)
+# while True:
+#     cv2.imshow('Image',img)
 
-
-while True:
-
-    cv2.setMouseCallback("Image", mousePoints)
-    cv2.imshow('Image',img)
-    if cv2.waitKey(1) and 0xFF == ord('q'):
-        pointsList = []
-        img = cv2.imread(path)
+#     # cv2.setMouseCallback("Image", mousePoints)
+    
+#     if cv2.waitKey(1) and 0xFF == ord('q'):
+#         pointsList = []
+#         img = cv2.imread(path)
 
